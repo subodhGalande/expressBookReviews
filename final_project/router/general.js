0f993a4a997bc0cbf8ef69d88c42f2cb99c181a7
Galande <subodh.galande@capgemini.com>
2023-09-12 17:42:02 +0530

    task 2 has been completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,13 +19,18 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  let filtered_isbn = books[isbn];
+  res.send(filtered_isbn);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author_param = req.params.author;
+  let author = Object.keys(author);
+  console.log(author);
+  
 });
 
 // Get all books based on title
