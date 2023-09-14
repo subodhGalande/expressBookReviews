2ec5f07e3d24e14d6ca01070141d7cf4bcd11a1c
Galande <subodh.galande@capgemini.com>
2023-09-14 12:39:05 +0530

    completed till task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,7 +13,7 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+ 
 });
 
 // Get book details based on ISBN
@@ -28,21 +28,52 @@ public_users.get('/isbn/:isbn',function (req, res) {
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
   const author_param = req.params.author;
-  let author = Object.keys(author);
-  console.log(author);
+  let booklist = Object.keys(books);
+  for (let i = 1; i<booklist.length; i++){
+    let authorbyISBN = books[i];
+    if (authorbyISBN.author === author_param ){
+      return res.status(300).json({booksbyAuthor: [
+        {isbn: i,
+         title: authorbyISBN.title,
+         reviews: authorbyISBN.reviews}
+        
+      ]});
+    }  
+    
+  }
+  
+  
+ 
+  
   
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
+  const title_param = req.params.title;
+  let booklist = Object.keys(books);
+  for (let i = 1; i<booklist.length; i++){
+    let titleByIsbn = books[i];
+    if (titleByIsbn.title === title_param ){
+      return res.status(300).json({booksbyTitle: [
+        {isbn: i,
+         Author: titleByIsbn.author,
+         reviews: titleByIsbn.reviews}
+        
+      ]});
+    }
+  }
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+ 
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  let filtered_isbn = books[isbn];
+  res.send(filtered_isbn.reviews);
+  
 });
 
 module.exports.general = public_users;
